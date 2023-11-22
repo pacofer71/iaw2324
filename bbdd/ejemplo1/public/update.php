4fd662ba047311937ca4ddef82ab8e236c5cff1f
Francisco Fernández-Arévalo Collado <pacofer71@gmail.com>
2023-11-22 09:26:35 +0100

    Dia 22/11/23

--- bbdd/ejemplo1/public/update.php
+++ bbdd/ejemplo1/public/update.php
@@ -6,6 +6,7 @@
             unset($_SESSION[$nombre]);
         }
     }
+
     if(!isset($_GET['id'])){
         header("Location:datos.php");
         die();
@@ -23,6 +24,53 @@
     mysqli_stmt_execute($stmt);
     mysqli_stmt_bind_result($stmt, $id, $email, $descripcion, $pass);
     mysqli_stmt_fetch($stmt);
+    //Ahora vemos si nos han mandado el form y lo procesamos
+    if(isset($_POST['btn'])){
+        $email=htmlspecialchars(trim($_POST['email']));
+        $descripcion=htmlspecialchars(trim($_POST['descripcion']));
+        $pass=htmlspecialchars(trim($_POST['pass']));
+
+        $errores=false;
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $errores=true;
+            $_SESSION['Email']="*** Error el email NO es válido";
+        }else{
+            //el email es valido, vamos a ver si ya lo tiene otro usuario
+            $q="select * from usuarios where email=? AND id != ?";
+            $stmt=mysqli_stmt_init($llave);
+            mysqli_stmt_prepare($stmt, $q);
+            mysqli_stmt_bind_param($stmt, 'si', $email, $idUser);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+            if(mysqli_stmt_num_rows($stmt)!=0){
+                $errores=true;
+                $_SESSION['Email']="*** Error, el email ya está dado de alta";
+            }
+            mysqli_stmt_close($stmt);
+        }
+        if(strlen($pass)<5 || strlen($pass)>32){
+            $errores=true;
+            $_SESSION['Pass']="*** Error la contraseña debe tener entre 5 y 32 caracteres";
+        }
+        if(strlen($descripcion)<10 || strlen($descripcion)>200){
+            $errores=true;
+            $_SESSION['Descripcion']="*** Error la descripcion debe tener entre 10 y 200 caracteres";
+        }
+
+        if($errores){
+            header("Location:update.php?id=$idUser");
+            die();
+        }
+        //si he llegado aqui los datos son correctos procedemos a actualizar el usuario
+        $q="update usuarios set email=?, pass=?, descripcion=? where id=?";
+        $stmt=mysqli_stmt_init($llave); // NO uso $stmt=mysqli_prepare($llave, $q) pq ya lo use arriba
+        mysqli_stmt_prepare($stmt, $q);
+        mysqli_stmt_bind_param($stmt, 'sssi', $email, $pass, $descripcion, $idUser);
+        mysqli_stmt_execute($stmt);
+        mysqli_close($llave);
+        header("Location:datos.php");
+
+    }
 
 ?>
 <!DOCTYPE html>
